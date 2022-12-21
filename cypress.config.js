fdc69396ed9f3e50f62e47cdfdaa41034924dd83
Luminita Leu <luminitaleu44@gmail.com>
2022-12-21 17:41:37 -0500

    chore: configured Cypress framework

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  videoUploadOnPasses: false,
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/[hash]-junit.xml',
+    toConsole: false,
+  },
+  viewportHeight: 1000,
+  viewportWidth: 1900,
+  defaultCommandTimeout: 15000,
+  requestTimeout: 30000,
+  responseTimeout: 60000,
+  numTestsKeptInMemory: 15,
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+    },
+    specPattern: 'src/specs/**/*.cy.{js,jsx}',
+  }
+});
